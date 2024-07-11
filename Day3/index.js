7a9dff6b1190641be0840cfa5ed60b9a345b4527
vignesh <vigneshfornavy@gmail.com>
2024-07-11 18:22:19 +0530

    with rendering jsx

--- Day3/index.js
+++ Day3/index.js
@@ -5,6 +5,9 @@ import ReactDOM from "react-dom";
 
 const heading = React.createElement("h1",{},"Hello This is vignesh");
 
+//this babel will converts the jsx into react create element
+const jsxHead = <h2>Hellow</h2>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHead);
