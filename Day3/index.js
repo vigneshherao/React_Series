0754d2631b7f5628fbcfb1cafb74de4cdb7ac2ff
vignesh <vigneshfornavy@gmail.com>
2024-07-11 18:41:30 +0530

    Added The functional componet

--- Day3/index.js
+++ Day3/index.js
@@ -8,6 +8,15 @@ const heading = React.createElement("h1",{},"Hello This is vignesh");
 //this babel will converts the jsx into react create element
 const jsxHead = <h2>Hellow</h2>
 
+const BoxHeadComponent = () =>{
+    return (
+        <div>
+            <h2>Box Component</h2>
+            <p>This is paragraph</p>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHead);
+root.render(<BoxHeadComponent/>);
