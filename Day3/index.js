e8f4063e07936420033fdc32a523844da1faf0f0
vignesh <vigneshfornavy@gmail.com>
2024-07-11 18:20:32 +0530

    inital setup with creating create react app

---
+++ Day3/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+const heading = React.createElement("h1",{},"Hello This is vignesh");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
