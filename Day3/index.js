b6df136d92871c2fbfd03a0565f572659f1e0cd2
vignesh <vigneshfornavy@gmail.com>
2024-07-11 18:43:53 +0530

    Component inside component is called component composition

--- Day3/index.js
+++ Day3/index.js
@@ -8,11 +8,18 @@ const heading = React.createElement("h1",{},"Hello This is vignesh");
 //this babel will converts the jsx into react create element
 const jsxHead = <h2>Hellow</h2>
 
+const ButtonComponent = ()=>{
+    return <button>Click me</button>
+}
+//Component inside component is called component composion
+
 const BoxHeadComponent = () =>{
     return (
         <div>
             <h2>Box Component</h2>
             <p>This is paragraph</p>
+    
+            <ButtonComponent/>
         </div>
     )
 }
