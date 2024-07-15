1983f94a3dd7498bdc3b6a672d32dd2c04e85f4a
vignesh <vigneshfornavy@gmail.com>
2024-07-15 12:45:38 +0530

    Day4 inital commit

---
+++ Day4/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1",{},"Hello");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
