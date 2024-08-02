5370b6172c9a2583e0820ba94ca3115db068b79b
vignesh <vigneshfornavy@gmail.com>
2024-08-02 18:36:30 +0530

    Implemented Children and Head as  comom

--- Day7/Day6/src/component/Element.js
+++ Day7/Day6/src/component/Element.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Element = () => {
   return (
-    <div>Login Page</div>
-  )
+    <div className="login-container">
+      <h2>Login Page</h2>
+      <form className="login-form">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Element
+export default Element;
--- Day7/Day6/src/index.js
+++ Day7/Day6/src/index.js
@@ -2,49 +2,49 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import HeadComponent from "./component/HeadComponent";
 import BodyComponent from "./component/BodyComponent";
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Element from "./component/Element";
 import Error from "./component/Error";
-
+import { Outlet } from "react-router-dom";
 
 //Creating react element with react core
-const heading = React.createElement("h1",{},"Hello This is vignesh");
+const heading = React.createElement("h1", {}, "Hello This is vignesh");
 
 //same thing using jsx
 
 const head = <h1>This is vignesh H E</h1>;
 
-//React has the root which is called head node which renders in dom 
+//React has the root which is called head node which renders in dom
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+//creating the functional component
 
+const AppComponent = () => {
+  return (
+    <>
+      <HeadComponent />
+      <Outlet/>
+    </>
+  );
+};
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppComponent />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <BodyComponent />,
+      },
+      {
+        path: "/login",
+        element: <Element />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
-//creating the functional component 
-
-const AppComponent = () =>{
-    return (
-        <>
-         <HeadComponent/>
-         <BodyComponent/>
-        </>
-    )
-}
-
-const appRouter = createBrowserRouter(
-    [
-        {
-            path:"/",
-            element:<AppComponent/>,
-            errorElement:<Error/>
-
-        },
-        {
-            path:"/login",
-            element:<Element/>
-        }
-    ]
-)
-
-
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);
