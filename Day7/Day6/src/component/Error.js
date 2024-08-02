589e85c06652cdae512800b0775219bc70de439d
vignesh <vigneshfornavy@gmail.com>
2024-08-02 16:53:06 +0530

    Implement Router Error Component

--- Day7/Day6/src/component/Error.js
+++ Day7/Day6/src/component/Error.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
+
+    const err = useRouteError();
+    console.log(err);
+
+
   return (
     <div className="error-container">
       <div className="error-icon">😞</div>
       <h1 className="error-title">Oops! Something went wrong.</h1>
-      <p className="error-message">We couldn't find the page you're looking for.</p>
+      <p className="error-message">{err?.error?.message}</p>
     </div>
   );
 };
